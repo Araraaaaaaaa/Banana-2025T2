58f9eb5b3a686c71fb03b7ec60c8ef7dc11e49f4
Nico_ <118087492+Araraaaaaaaa@users.noreply.github.com>
2025-04-10 12:39:37 +0000

    Atividades concluidas

--- Aula_02/ativ1.py
+++ Aula_02/ativ1.py
@@ -1,1 +1,4 @@
 #uhhuiguguidgududegu
+a = str(input("Escreva seu nome: "))
+b = int(input("Escreva a sua matrícula: "))
+print(f"Aluno(a) {a} da matrícula {b}")
---
+++ Aula_02/ativ2.py
@@ -0,0 +1,2 @@
+a = print("Como deseja ser chamado?  ")
+print(f"Bem-vindo(a) ao Codespaces, {a}")
