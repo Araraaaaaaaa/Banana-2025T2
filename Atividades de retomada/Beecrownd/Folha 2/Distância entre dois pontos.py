ff1815c318b9776830ed8060fca654500f892893
Nico_ <118087492+Araraaaaaaaa@users.noreply.github.com>
2025-04-18 00:20:09 +0000

    ultimo comit do dia

--- Atividades de retomada/Beecrownd/Folha 2/Distância entre dois pontos.py
+++ Atividades de retomada/Beecrownd/Folha 2/Distância entre dois pontos.py
@@ -3,11 +3,12 @@ import math
 def funcionOfBanana(valores):
     for indice, valor in enumerate(valores):
         if valor == " ":
-            X , Y = float(valores[:indice-1]), float(valores[:indice-2])
+            X , Y = float(valores[:indice-1]), float(valores[indice+1:])
+    print(X, Y)        
     return X, Y
 
-lote1 , lote2 = str(input()) , str(input()) #não consegui puxar pelo input como float, estava dando o erro seguinte => ValueError: could not convert string to float: '1.0 7.0'
+lote1 , lote2 = str(input()) , str(input()) #não consegui puxar pelo input como float, estava dando o erro seguinte => ValueError: could not convert string to float: '1.0 7.0', pois, usando o exemplo do site -sem vírgulas-, o python não consegue diferenciar os termos
 X1, Y1 = funcionOfBanana(lote1)
 X2, Y2 = funcionOfBanana(lote2)
-
-print( math.sqrt(((X2-X1)**2)+((Y2-Y1)**2)) ) #não consigo colocar o :.4f
+conta = math.sqrt((X2 - X1)**2+(Y2 - Y1)**2)
+print(f"{conta:.4f}")
--- Atividades de retomada/Beecrownd/Folha 2/Tomadas.py
+++ Atividades de retomada/Beecrownd/Folha 2/Tomadas.py
@@ -1,1 +1,15 @@
 '''https://www.beecrowd.com.br/repository/UOJ_1930.html'''
+def funcionOfBanana(Tomadas, emp1, emp2, emp3):
+    for indice, valor in enumerate(Tomadas):
+        if emp1 != 0 and empty2 != 0 and empty3 != 0:
+            T1, T2, T3, T4 = int(Tomadas[:emp1-1]), int(Tomadas[emp1+1:empty2-1]), int(Tomadas[empty2+1:empty3-1]), int(Tomadas[empty3+1:])
+        if valor == " ":
+            if emp1 == 0:
+                emp1 = indice
+            elif empty2 == 0:
+                empty2 = indice
+            elif empty3 == 0:
+                empty3 = indice
+Tomad, empty1, empty2, empty3 = str(input()), 0, 0, 0
+ 
+print((T1-1)+(T2-1)+(T3-1)+T4)
