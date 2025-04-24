b10b249dce171717518b1ed87b6836c7defd5056
Nico_ <118087492+Araraaaaaaaa@users.noreply.github.com>
2025-04-24 13:24:07 +0000

    sinc

--- Atividades de retomada/Lista5/quinto.py
+++ Atividades de retomada/Lista5/quinto.py
@@ -2,7 +2,7 @@
 usando a função formatar_nome abaixo. O programa deve solicitar do usuário seu nome e mostrá-lo formatado.
 def formatar_nome(nome)'''
 
-def formatar_nome(nome):
+'''def formatar_n(nome):
     nome, Chave, nome_format = nome.lower(), True, ''
     for valor in nome: # varre as letras
         if Chave:
@@ -14,4 +14,19 @@ def formatar_nome(nome):
             nome_format = nome_format + valor
         
     return nome_format
-print(formatar_nome(input()))
+print(formatar_n(input()))'''
+
+'''Feito em sala:'''
+
+def formatar_nome(name):
+    name = name.lower()
+    name = name.split()
+    Resposta = ""
+    for palavra in name:
+        if palavra in ["da", "de", "e", "dos", "das", "do"]:
+            Resposta = Resposta + palavra + " "
+        else:
+            Resposta = Resposta + (palavra[0].upper() + palavra[1:]) + " "
+    return Resposta
+print(formatar_nome(input("Digite o seu nome: ")))
+
