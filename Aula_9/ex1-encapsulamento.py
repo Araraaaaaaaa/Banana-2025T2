e0964c96f53358a015e76f98b98aaa259c977bb7
Nico_ <118087492+Araraaaaaaaa@users.noreply.github.com>
2025-05-22 13:23:42 +0000

    comit fim de aula

---
+++ Aula_9/ex1-encapsulamento.py
@@ -0,0 +1,31 @@
+class circulo:
+    def __init__(self):
+        self.__raio = 1.2
+        self.__pi = 3.14
+    def validar_raio(self, raio):
+        if not raio > 0: raise ValueError
+        self.__raio = raio
+    def validar_pi(self, pi):
+        if not pi >= 3 and pi <= 3.2: raise ValueError
+        self.__pi = pi
+    def pegar_raio(self): return self.__raio
+    def pegar_pi(self): return self.__pi
+    
+    def area (self):
+        result = self.__pi * self.__raio**3
+        return result
+    def circuferencia(self):
+        result = 2 * self.__pi * self.__raio
+        return result
+    
+
+class interface:
+    @staticmethod
+    def main():
+        Y = circulo()
+        Y.__raio = float(input())
+        Y.__pi = 3.14
+        print(Y.area())
+        print(Y.circuferencia())
+
+interface.main()
---
+++ corrigindo prova/1-bimestre1.py
@@ -0,0 +1,9 @@
+# x = 10 
+# y = 5
+# print((x < 5 and y < 10) or x == 10)
+
+k = 18
+s = -1
+for d in range (1,30):
+    if k % d == 0: print(s * d, end=" ")
+    s = -s
