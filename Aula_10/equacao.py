1136131f941fe0396c9675f2d4a8562c3f740565
Nico_ <118087492+Araraaaaaaaa@users.noreply.github.com>
2025-06-02 21:28:12 +0000

    commit fim de dia

--- Aula_10/equacao.py
+++ Aula_10/equacao.py
@@ -1,24 +1,28 @@
 import math
 class equacao:
-    def __init__( self , A, B, C):
+    def __init__ ( self , A, B, C ):
         self.set__A( A )
         self.set__B( B )
         self.set__C( C )
     def set__A ( self, A ):
-        if A == 0: raise ValueError("O primeiro valor não pode ser negativo")
+        if A == 0: raise ValueError("O primeiro valor não pode ser nulo")
         self.__A = A
     def set__B ( self, B ): self.__B = B
     def set__C ( self, C ): self.__C = C
     def get__A ( self ): return self.__A
     def get__B ( self ): return self.__B
     def get__C ( self ): return self.__C
-    def Delta ( self ): return (self.__B**2)*(-4*self.__A*self.__C)
+    def Delta ( self ): return self.__B**2 + (-4*self.__A*self.__C)
     def temRaizReal( self ):
         if self.Delta() >= 0: return True
         else: return False
-    def R1 ( self ): return (-self.__B - (math.sqrt(self.Delta())))/2* self.__A
-    def R2 ( self ): return (-self.__B + (math.sqrt(self.Delta())))/2* self.__A
-    def __init__ ( self ):
+    def R1 ( self ): 
+        if self.temRaizReal() == False: return "Operação inválida"
+        else: return (-self.__B - (math.sqrt(self.Delta())))/2* self.__A      
+    def R2 ( self ): 
+        if self.temRaizReal == False: return "Operação inválida" 
+        else: return (-self.__B + (math.sqrt(self.Delta())))/2* self.__A
+    def __str__ ( self ):
         return f"Valores - A = {self.__A} | B = {self.__B} | C = {self.__C}"
         
 class UI:
@@ -31,14 +35,14 @@ class UI:
             print("0- encerrar | 1- Raiz tipo 1 | 2- Raiz tipo 2 | 3- Mudar valores | 4- Tipo da Raiz | 5- Delta | 6- Atributos")
             l = int(input())
             match l:
-                case 1: H.R1()
-                case 2: H.R2()
+                case 1: print(f"Raiz 1 = { H.R1() }")
+                case 2: print(f"Raiz 2 = { H.R2() }")
                 case 3: 
                     H.set__A(float(input(f"Qual o novo valor de A [{H.get__A()}]? ")))
                     H.set__B(float(input(f"Qual o novo valor de B [{H.get__B()}]? ")))
                     H.set__C(float(input(f"Qual o novo valor de C [{H.get__C()}]? ")))
                 case 4: print(f"A afirmação de que as raizes são reais é {H.temRaizReal()}")
-                case 5: H.Delta()
+                case 5: print(f"Esse é o delta da sua equação do segundo grau --> {H.Delta()}")
                 case 6: print(H)
                 case 0: break;  
 UI.main()
