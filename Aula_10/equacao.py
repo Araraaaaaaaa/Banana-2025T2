56417db5f4faf633ec95a32c8d5bd38be508b2ed
Nico_ <118087492+Araraaaaaaaa@users.noreply.github.com>
2025-06-02 21:02:33 +0000

    atualizacao basica

--- Aula_10/equacao.py
+++ Aula_10/equacao.py
@@ -1,20 +1,25 @@
+import math
 class equacao:
     def __init__( self , A, B, C):
         self.set__A( A )
         self.set__B( B )
         self.set__C( C )
     def set__A ( self, A ):
-    def set__B ( self, B ):
-    def set__C ( self, C ):
+        if A == 0: raise ValueError("O primeiro valor não pode ser negativo")
+        self.__A = A
+    def set__B ( self, B ): self.__B = B
+    def set__C ( self, C ): self.__C = C
     def get__A ( self ): return self.__A
     def get__B ( self ): return self.__B
     def get__C ( self ): return self.__C
-    def Delta ( self ):
+    def Delta ( self ): return (self.__B**2)*(-4*self.__A*self.__C)
     def temRaizReal( self ):
-    def R1 ( self ):
-    def R2 ( self ):
-    def valores ( self ):
-        X, Y, Z = float(input("Qual o valor de A? ")), float(input("Qual o valor de B? ")), float(input("Qual o valor de C? "))
+        if self.Delta() >= 0: return True
+        else: return False
+    def R1 ( self ): return (-self.__B - (math.sqrt(self.Delta())))/2* self.__A
+    def R2 ( self ): return (-self.__B + (math.sqrt(self.Delta())))/2* self.__A
+    def __init__ ( self ):
+        return f"Valores - A = {self.__A} | B = {self.__B} | C = {self.__C}"
         
 class UI:
     def main():
@@ -23,12 +28,17 @@ class UI:
         
         while l != 0:
             print("  ")
-            print("0- encerrar | 1- Mudar valores | 2- Mudar distancia | 3- Calcular frete | 4- Atributos")
+            print("0- encerrar | 1- Raiz tipo 1 | 2- Raiz tipo 2 | 3- Mudar valores | 4- Tipo da Raiz | 5- Delta | 6- Atributos")
             l = int(input())
             match l:
-                case 1: H.set__peso(float(input(f"Mudar peso de {H.get__peso()} para: ")))
-                case 2: H.set__distancia(float(input(f"Mudar distancia de {H.get__distancia()} para: ")))
-                case 3: print(f"Valor do frete: R${H.CalcFrete()}")
-                case 4: print(H)
+                case 1: H.R1()
+                case 2: H.R2()
+                case 3: 
+                    H.set__A(float(input(f"Qual o novo valor de A [{H.get__A()}]? ")))
+                    H.set__B(float(input(f"Qual o novo valor de B [{H.get__B()}]? ")))
+                    H.set__C(float(input(f"Qual o novo valor de C [{H.get__C()}]? ")))
+                case 4: print(f"A afirmação de que as raizes são reais é {H.temRaizReal()}")
+                case 5: H.Delta()
+                case 6: print(H)
                 case 0: break;  
 UI.main()
