2189f7f8ff8f4c0151310c1ac6d0e3f5efef153e
Nico_ <118087492+Araraaaaaaaa@users.noreply.github.com>
2025-09-16 14:37:32 +0000

    socorro

--- Semestre2/Paciente./index.py
+++ Semestre2/Paciente./index.py
@@ -6,13 +6,14 @@ class Paciente:
     #______--__- Estado inicial da página
     def main():
         st.header("Dados do paciente")
-        Nome = st.text.input("Nome")
-        Cpf = st.text.input("CPF")
-        Fone = st.text.input("Fone")
-        Nascimento = st.text.input("Nascimento")
+        Nome = st.text_input("Nome")
+        Cpf = st.text_input("CPF")
+        Fone = st.text_input("Fone")
+        Nascimento = st.text_input("Nascimento")
 
         #______--__- Resposta do sistema
         if st.button("Idade"):
             objeto = PPaciente(Nome, Cpf, Fone, datetime.strptime(Nascimento, "d%/m%/Y%"))
             st.write(objeto)
             st.write(objeto.idade())
+Paciente.main()
