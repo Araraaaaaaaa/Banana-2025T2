6a34fffae1a34ecf14f1aeef90b1ecac6d6754ce
Nico_ <118087492+Araraaaaaaaa@users.noreply.github.com>
2025-10-05 20:41:28 +0000

    sinc

--- SistemaAgendamento/templates/loginUI.py
+++ SistemaAgendamento/templates/loginUI.py
@@ -12,9 +12,9 @@ class LoginUI:
             p = View.profissional_autenticar(email, senha)
 
             if p is not None:
-                st.session_state["usuario_id"] = c["id"]
-                st.session_state["usuario_nome"] = c["nome"]
-                st.session_state["usuario_email"] = c["email"]
+                st.session_state["usuario_id"] = p["id"]
+                st.session_state["usuario_nome"] = p["nome"]
+                st.session_state["usuario_email"] = p["email"]
                 st.session_state["usuario_tipo"] = "profissional"
                 st.rerun()
 
